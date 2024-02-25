71312c417cd51caed3ef2019ef928f2752b41100
BuildTools <unconfigured@null.spigotmc.org>
2024-02-25 22:18:09 +0800

    Fix bugs

--- src/main/java/ru/javatest/javautility/util/FileUtils.java
+++ src/main/java/ru/javatest/javautility/util/FileUtils.java
@@ -38,6 +38,7 @@ public class FileUtils {
         File[] listFiles = file.listFiles();
         if(listFiles == null) return Optional.empty();
 
+
         List<File> files = new ArrayList<>();
 
         for (File dirFile : listFiles)
